5af2fd751eebdb644f774c5cbe0c07aff2f3b63c
dtracydepp <tracydepp82@gmail.com>
2021-05-12 15:30:11 -0500

    added eventview class and updated urls.py

--- levelup/urls.py
+++ levelup/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from levelupapi.views.event import EventView
+from levelupapi.models.event import Event
 from levelupapi.views.game import GameView
 from django.contrib import admin
 from django.urls import path
@@ -23,11 +25,13 @@ from levelupapi.views import register_user, login_user
 from rest_framework import routers
 from levelupapi.views import GameTypeView
 from levelupapi.views import gametype, game
+from levelupapi.views import event
 
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypeView, 'gametype')
 router.register(r'games', GameView, 'game')
+router.register(r'events', EventView, 'event')
 
 urlpatterns = [
     path('', include(router.urls)),
